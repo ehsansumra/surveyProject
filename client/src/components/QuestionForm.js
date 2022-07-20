25c3be195cabc0d48bcc14ba9bc0c7d1b14ce1c4
Ehsan Sumra <ehsansumra@gmail.com>
2022-07-19 21:36:15 -0400

    Solved the Question delete issue. Cleanup required

--- client/src/components/QuestionForm.js
+++ client/src/components/QuestionForm.js
@@ -10,20 +10,46 @@ import { SButton } from "./SButton";
 import "../styles/QuestionForm.css";
 
 const QuestionForm = (props) => {
+    // components is an array of AnswerBox components, Used to track and alter the display and # of answer fields on the screen
     const [components, setComponents] = useState([]);
 
+    const [question, setQuestion] = useState("");
+
+    const [answers, setAnswers] = useState([])
+
+
+    const createAnswerBox = (answer, index) => {
+        return (
+            <InputGroup>
+                <Form.Control placeholder="Answer" style={fieldStyle} value={answer} onChange={(e) => updateInput(e, index)}  />
+                <SButton onClick={() => deleteAnswer(index)} style={fieldStyle} variant="secondary" className="edit-button" text="Delete" />
+            </InputGroup>
+        )
+    }
+
+    const updateInput = (e, index) => {
+        const newArr = [...answers];
+        newArr[index] = e.target.value;
+
+        setAnswers(newArr)
+    }
+
+    const addAnswer = () => {
+        setAnswers([...answers, ""])
+    }
+
+    const deleteAnswer = (index) => {
+        const newArr = [...answers];
+        newArr.splice(index, 1)
+
+        setAnswers(newArr)
+    }
+
+    // update components state with another AnswerBox
     const addComponent = () => {
-        console.log("yo")
-        let res = []
-        let i = 0;
-
-        while (i < components.length) {
-            res.push(components[i])
-            i++
-        }
-
-        res.push(<AnswerBox index={i}/>)
-        setComponents(res)
+        let len = components.length;
+        setComponents([...components, <AnswerBox key={len} index={len}/>])
+        console.log(components)
     }
 
     //TODO: try deleting specific components depending on button press.
@@ -31,41 +57,57 @@ const QuestionForm = (props) => {
         setComponents([...components].slice(0, components.length - 1));
     }
 
+    // remove an AnswerBox from components state.
     const handleRemove = (idx) => {
         console.log("key " + idx)
         console.log(components)
-        setComponents(components.filter((item, index)=> index !== idx))
+        const newArr = [...components]
+
+        setComponents(newArr)
+
+
     }
 
-    let fieldStyle = {marginBottom: "15px", borderRadius:"0"}
+    let fieldStyle = { marginBottom: "15px", borderRadius: "0" }
 
-    const AnswerBox = ({index}) => {
+    // this is being tracked by components state.
+    // contains a button that calls handleRemove
+    const AnswerBox = ({ index}) => {
         return (
             <InputGroup>
                 <Form.Control placeholder="Answer" style={fieldStyle} />
-                <SButton onClick={() => handleRemove(index)} style={fieldStyle} variant="outline-secondary" className="edit-button" text="Delete" />
+                <SButton onClick={() => handleRemove(index)} style={fieldStyle} variant="secondary" className="edit-button" text="Delete" />
             </InputGroup>
         )
     }
 
+    //Prevent the the default refresh of the form
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     setAnswers([...components.value]);
+    //     console.log(answers)
+
+    //     console.log("handleSubmit call")
+    // }
+
     return (
-        <Card className="question-card" style={{ width: "600px", backgroundColor: "#F1F1F1"}}>
+        <Card className="question-card" style={{ width: "600px", backgroundColor: "#F1F1F1" }}>
             <Card.Body>
 
-                <Form>
-                    
+                <Form >
+
                     <div className="edit-container">
-                        <SButton onClick={addComponent} variant="secondary" className="edit-button" text="Add answer" />
+                        <SButton onClick={addAnswer} variant="secondary" className="edit-button" text="Add answer" />
                     </div>
 
                     <Form.Group>
                         <Form.Label>{props.label}</Form.Label>
-                        <Form.Control placeholder="Question" style={fieldStyle}></Form.Control>
+                        <Form.Control value={question} onChange={(e) => setQuestion(e.target.value)} placeholder="Question" style={fieldStyle}></Form.Control>
                     </Form.Group>
 
                     <Form.Group>
-                        <AnswerBox/>
-                        {components.map((item, i) => (item))}
+
+                        {answers.map((answer, i) => (createAnswerBox(answer, i)))}
                     </Form.Group>
 
                     <div className="edit-container">
